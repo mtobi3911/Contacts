857e9f751c71b04540b8aa988c8e5fba32b1b684
mtobi3911 <mtobi3911@gmail.com>
2024-04-08 14:35:22 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Никита Курбатов on 08.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
